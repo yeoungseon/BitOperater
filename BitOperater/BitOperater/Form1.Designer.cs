5fce83de1ea8d0a77b8135e6f1b611506ff820d6
yeoungseonseok <young020920@gmail.com>
2022-11-05 22:06:16 +0900

    initial commit

---
+++ BitOperater/BitOperater/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace BitOperater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnToBit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chk1 = new System.Windows.Forms.CheckBox();
+            this.chk0 = new System.Windows.Forms.CheckBox();
+            this.chk7 = new System.Windows.Forms.CheckBox();
+            this.chk5 = new System.Windows.Forms.CheckBox();
+            this.chk3 = new System.Windows.Forms.CheckBox();
+            this.chk2 = new System.Windows.Forms.CheckBox();
+            this.chk6 = new System.Windows.Forms.CheckBox();
+            this.chk4 = new System.Windows.Forms.CheckBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtIndex = new System.Windows.Forms.TextBox();
+            this.btnFromBit = new System.Windows.Forms.Button();
+            this.btnOn = new System.Windows.Forms.Button();
+            this.btnOff = new System.Windows.Forms.Button();
+            this.btnToggle = new System.Windows.Forms.Button();
+            this.btnShiftUp = new System.Windows.Forms.Button();
+            this.btnShiftDown = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnToBit
+            // 
+            this.btnToBit.Location = new System.Drawing.Point(229, 38);
+            this.btnToBit.Name = "btnToBit";
+            this.btnToBit.Size = new System.Drawing.Size(98, 50);
+            this.btnToBit.TabIndex = 0;
+            this.btnToBit.Text = ">>";
+            this.btnToBit.UseVisualStyleBackColor = true;
+            this.btnToBit.Click += new System.EventHandler(this.btnToBit_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chk1);
+            this.groupBox1.Controls.Add(this.chk0);
+            this.groupBox1.Controls.Add(this.chk7);
+            this.groupBox1.Controls.Add(this.chk5);
+            this.groupBox1.Controls.Add(this.chk3);
+            this.groupBox1.Controls.Add(this.chk2);
+            this.groupBox1.Controls.Add(this.chk6);
+            this.groupBox1.Controls.Add(this.chk4);
+            this.groupBox1.Location = new System.Drawing.Point(378, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(117, 361);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bits";
+            // 
+            // chk1
+            // 
+            this.chk1.AutoSize = true;
+            this.chk1.Location = new System.Drawing.Point(25, 268);
+            this.chk1.Name = "chk1";
+            this.chk1.Size = new System.Drawing.Size(30, 16);
+            this.chk1.TabIndex = 16;
+            this.chk1.Text = "1";
+            this.chk1.UseVisualStyleBackColor = true;
+            // 
+            // chk0
+            // 
+            this.chk0.AutoSize = true;
+            this.chk0.Location = new System.Drawing.Point(25, 302);
+            this.chk0.Name = "chk0";
+            this.chk0.Size = new System.Drawing.Size(30, 16);
+            this.chk0.TabIndex = 17;
+            this.chk0.Text = "0";
+            this.chk0.UseVisualStyleBackColor = true;
+            // 
+            // chk7
+            // 
+            this.chk7.AutoSize = true;
+            this.chk7.Location = new System.Drawing.Point(25, 32);
+            this.chk7.Name = "chk7";
+            this.chk7.Size = new System.Drawing.Size(30, 16);
+            this.chk7.TabIndex = 0;
+            this.chk7.Text = "7";
+            this.chk7.UseVisualStyleBackColor = true;
+            // 
+            // chk5
+            // 
+            this.chk5.AutoSize = true;
+            this.chk5.Location = new System.Drawing.Point(25, 104);
+            this.chk5.Name = "chk5";
+            this.chk5.Size = new System.Drawing.Size(30, 16);
+            this.chk5.TabIndex = 12;
+            this.chk5.Text = "5";
+            this.chk5.UseVisualStyleBackColor = true;
+            this.chk5.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // chk3
+            // 
+            this.chk3.AutoSize = true;
+            this.chk3.Location = new System.Drawing.Point(25, 183);
+            this.chk3.Name = "chk3";
+            this.chk3.Size = new System.Drawing.Size(30, 16);
+            this.chk3.TabIndex = 14;
+            this.chk3.Text = "3";
+            this.chk3.UseVisualStyleBackColor = true;
+            this.chk3.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // chk2
+            // 
+            this.chk2.AutoSize = true;
+            this.chk2.Location = new System.Drawing.Point(25, 230);
+            this.chk2.Name = "chk2";
+            this.chk2.Size = new System.Drawing.Size(30, 16);
+            this.chk2.TabIndex = 15;
+            this.chk2.Text = "2";
+            this.chk2.UseVisualStyleBackColor = true;
+            // 
+            // chk6
+            // 
+            this.chk6.AutoSize = true;
+            this.chk6.Location = new System.Drawing.Point(25, 70);
+            this.chk6.Name = "chk6";
+            this.chk6.Size = new System.Drawing.Size(30, 16);
+            this.chk6.TabIndex = 11;
+            this.chk6.Text = "6";
+            this.chk6.UseVisualStyleBackColor = true;
+            // 
+            // chk4
+            // 
+            this.chk4.AutoSize = true;
+            this.chk4.Location = new System.Drawing.Point(25, 142);
+            this.chk4.Name = "chk4";
+            this.chk4.Size = new System.Drawing.Size(30, 16);
+            this.chk4.TabIndex = 13;
+            this.chk4.Text = "4";
+            this.chk4.UseVisualStyleBackColor = true;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(27, 54);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(149, 21);
+            this.txtNumber.TabIndex = 3;
+            this.txtNumber.Text = "45";
+            // 
+            // txtIndex
+            // 
+            this.txtIndex.Location = new System.Drawing.Point(27, 196);
+            this.txtIndex.Name = "txtIndex";
+            this.txtIndex.Size = new System.Drawing.Size(149, 21);
+            this.txtIndex.TabIndex = 4;
+            this.txtIndex.Text = "3";
+            // 
+            // btnFromBit
+            // 
+            this.btnFromBit.Location = new System.Drawing.Point(229, 94);
+            this.btnFromBit.Name = "btnFromBit";
+            this.btnFromBit.Size = new System.Drawing.Size(98, 50);
+            this.btnFromBit.TabIndex = 5;
+            this.btnFromBit.Text = "<<";
+            this.btnFromBit.UseVisualStyleBackColor = true;
+            this.btnFromBit.Click += new System.EventHandler(this.btnFromBit_Click);
+            // 
+            // btnOn
+            // 
+            this.btnOn.Location = new System.Drawing.Point(229, 180);
+            this.btnOn.Name = "btnOn";
+            this.btnOn.Size = new System.Drawing.Size(98, 50);
+            this.btnOn.TabIndex = 6;
+            this.btnOn.Text = "ON";
+            this.btnOn.UseVisualStyleBackColor = true;
+            this.btnOn.Click += new System.EventHandler(this.btnOn_Click);
+            // 
+            // btnOff
+            // 
+            this.btnOff.Location = new System.Drawing.Point(229, 236);
+            this.btnOff.Name = "btnOff";
+            this.btnOff.Size = new System.Drawing.Size(98, 50);
+            this.btnOff.TabIndex = 7;
+            this.btnOff.Text = "OFF";
+            this.btnOff.UseVisualStyleBackColor = true;
+            this.btnOff.Click += new System.EventHandler(this.btnOff_Click);
+            // 
+            // btnToggle
+            // 
+            this.btnToggle.Location = new System.Drawing.Point(229, 292);
+            this.btnToggle.Name = "btnToggle";
+            this.btnToggle.Size = new System.Drawing.Size(98, 50);
+            this.btnToggle.TabIndex = 8;
+            this.btnToggle.Text = "Toggle";
+            this.btnToggle.UseVisualStyleBackColor = true;
+            this.btnToggle.Click += new System.EventHandler(this.btnToggle_Click);
+            // 
+            // btnShiftUp
+            // 
+            this.btnShiftUp.Location = new System.Drawing.Point(44, 373);
+            this.btnShiftUp.Name = "btnShiftUp";
+            this.btnShiftUp.Size = new System.Drawing.Size(132, 50);
+            this.btnShiftUp.TabIndex = 9;
+            this.btnShiftUp.Text = "ShiftUp";
+            this.btnShiftUp.UseVisualStyleBackColor = true;
+            this.btnShiftUp.Click += new System.EventHandler(this.btnShiftUp_Click);
+            // 
+            // btnShiftDown
+            // 
+            this.btnShiftDown.Location = new System.Drawing.Point(211, 373);
+            this.btnShiftDown.Name = "btnShiftDown";
+            this.btnShiftDown.Size = new System.Drawing.Size(132, 50);
+            this.btnShiftDown.TabIndex = 10;
+            this.btnShiftDown.Text = "ShiftDown";
+            this.btnShiftDown.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 467);
+            this.Controls.Add(this.btnShiftDown);
+            this.Controls.Add(this.btnShiftUp);
+            this.Controls.Add(this.btnToggle);
+            this.Controls.Add(this.btnOff);
+            this.Controls.Add(this.btnOn);
+            this.Controls.Add(this.btnFromBit);
+            this.Controls.Add(this.txtIndex);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnToBit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToBit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chk1;
+        private System.Windows.Forms.CheckBox chk0;
+        private System.Windows.Forms.CheckBox chk7;
+        private System.Windows.Forms.CheckBox chk5;
+        private System.Windows.Forms.CheckBox chk3;
+        private System.Windows.Forms.CheckBox chk2;
+        private System.Windows.Forms.CheckBox chk6;
+        private System.Windows.Forms.CheckBox chk4;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtIndex;
+        private System.Windows.Forms.Button btnFromBit;
+        private System.Windows.Forms.Button btnOn;
+        private System.Windows.Forms.Button btnOff;
+        private System.Windows.Forms.Button btnToggle;
+        private System.Windows.Forms.Button btnShiftUp;
+        private System.Windows.Forms.Button btnShiftDown;
+    }
+}
+
---
+++ BitOperater/BitOperater/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitOperater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SetCheckBox(int gB)
+        {
+            chk0.Checked = (gB & (0x01) << 0) != 0;
+            chk1.Checked = (gB & (0x01) << 1) != 0;
+            chk2.Checked = (gB & (0x01) << 2) != 0;
+            chk3.Checked = (gB & (0x01) << 3) != 0;
+            chk4.Checked = (gB & (0x01) << 4) != 0;
+            chk5.Checked = (gB & (0x01) << 5) != 0;
+            chk6.Checked = (gB & (0x01) << 6) != 0;
+            chk7.Checked = (gB & (0x01) << 7) != 0;
+        }
+
+        private int GetCheckBox()
+        {
+            int gB = 0;
+            if (chk0.Checked) gB = gB | 0x01;
+            if (chk1.Checked) gB = gB | 0x02;
+            if (chk2.Checked) gB = gB | 0x04;
+            if (chk3.Checked) gB = gB | 0x08;
+            if (chk4.Checked) gB = gB | 0x10;
+            if (chk5.Checked) gB = gB | 0x20;
+            if (chk6.Checked) gB = gB | 0x40;
+            if (chk7.Checked) gB = gB | 0x80;
+            return gB;
+        }
+
+
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnToBit_Click(object sender, EventArgs e)
+        {
+            int gB = Convert.ToInt32(txtNumber.Text);
+
+            //if ((gB & 0x01) != 0)
+            //    chk0.Checked = true;
+            //else
+            //    chk0.Checked = false;
+
+            //chk0.Checked = (gB & 0x01) != 0;
+            //chk1.Checked = (gB & 0x02) != 0;
+            //chk2.Checked = (gB & 0x04) != 0;
+            //chk3.Checked = (gB & 0x08) != 0;
+            //chk4.Checked = (gB & 0x10) != 0;
+            //chk5.Checked = (gB & 0x20) != 0;
+            //chk6.Checked = (gB & 0x40) != 0;
+            //chk7.Checked = (gB & 0x80) != 0;
+
+            //chk0.Checked = (gB & (0x01)<<0) != 0;
+            //chk1.Checked = (gB & (0x01) << 1) != 0;
+            //chk2.Checked = (gB & (0x01) << 2) != 0;
+            //chk3.Checked = (gB & (0x01) << 3) != 0;
+            //chk4.Checked = (gB & (0x01) << 4) != 0;
+            //chk5.Checked = (gB & (0x01) << 5) != 0;
+            //chk6.Checked = (gB & (0x01) << 6) != 0;
+            //chk7.Checked = (gB & (0x01) << 7) != 0;
+
+            SetCheckBox(gB);
+
+
+        }
+
+        private void btnFromBit_Click(object sender, EventArgs e)
+        {
+            int gB = GetCheckBox();
+            //if (chk0.Checked) gB = gB | 0x01;
+            //if (chk1.Checked) gB = gB | 0x02;
+            //if (chk2.Checked) gB = gB | 0x04;
+            //if (chk3.Checked) gB = gB | 0x08;
+            //if (chk4.Checked) gB = gB | 0x10;
+            //if (chk5.Checked) gB = gB | 0x20;
+            //if (chk6.Checked) gB = gB | 0x40;
+            //if (chk7.Checked) gB = gB | 0x80;
+
+            txtNumber.Text = Convert.ToString(gB);
+
+        }
+
+        private void btnOn_Click(object sender, EventArgs e)
+        {
+            int gB = Convert.ToInt32(txtNumber.Text);
+            int idx = Convert.ToInt32(txtIndex.Text);
+
+            gB = gB | (0x01 << idx);
+            txtNumber.Text = Convert.ToString(gB);
+
+            //chk0.Checked = (gB & (0x01) << 0) != 0;
+            //chk1.Checked = (gB & (0x01) << 1) != 0;
+            //chk2.Checked = (gB & (0x01) << 2) != 0;
+            //chk3.Checked = (gB & (0x01) << 3) != 0;
+            //chk4.Checked = (gB & (0x01) << 4) != 0;
+            //chk5.Checked = (gB & (0x01) << 5) != 0;
+            //chk6.Checked = (gB & (0x01) << 6) != 0;
+            //chk7.Checked = (gB & (0x01) << 7) != 0;
+
+            SetCheckBox(gB);
+        }
+
+        private void btnOff_Click(object sender, EventArgs e)
+        {
+            int gB = Convert.ToInt32(txtNumber.Text);
+            int idx = Convert.ToInt32(txtIndex.Text);
+
+            gB = gB & ~(0x01 << idx);
+            txtNumber.Text = Convert.ToString(gB);
+
+            SetCheckBox(gB);
+        }
+
+        private void btnShiftUp_Click(object sender, EventArgs e)
+        {
+            int gB = Convert.ToInt32(txtNumber.Text);
+            int idx = Convert.ToInt32(txtIndex.Text);
+
+            gB = (0x01 << idx)<<1;
+            txtNumber.Text = Convert.ToString(gB);
+
+            SetCheckBox(gB);
+        }
+
+        private void btnToggle_Click(object sender, EventArgs e)
+        {
+            int gB = Convert.ToInt32(txtNumber.Text);
+            int idx = Convert.ToInt32(txtIndex.Text);
+
+            gB = gB ^ ~(0x01 << idx);
+            txtNumber.Text = Convert.ToString(gB);
+
+            SetCheckBox(gB);
+        }
+    }
+}
